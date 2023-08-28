6487bd6a280b1a4d9046b9147ffe80c3933300eb
Yasha <yashar@rezilion.com>
2023-08-28 09:15:38 +0300

    Create ephemeral container for tests

--- workload_tests/test_workload.py
+++ workload_tests/test_workload.py
@@ -36,34 +36,40 @@ def connection():
     return _connection
 
 
+@pytest.fixture
+def container():
+    random_name = uuid.uuid4()
+    connection.create_container(random_name)
+    yield random_name
+    connection.delete_container(random_name)
+
+
 def run_workload_test(mean_size, max_size, min_size, stdev_size, retention_time,
-                     upload_interval, total_uploads, batch_size, connection):
+                     upload_interval, total_uploads, batch_size, connection, container):
    workloader = Workloader(mean_size, max_size, min_size, stdev_size,
                            retention_time, upload_interval, total_uploads,
                            batch_size, connection)
-   random_name = uuid.uuid4()
-   some_container = connection.create_container(random_name)
-   uploaded_objects = workloader.create_workload(some_container)
+   uploaded_objects = workloader.create_workload(container)
    verifier = Verifier()
    verifier.verify(uploaded_objects)
 
 
 # Continuously and slowly upload files without stopping, e.g. a live server that accepts occasional files from many clients
-def test_continuous_workload(connection):
+def test_continuous_workload(connection, container):
    run_workload_test(MEAN_SIZE, MAX_SIZE, MIN_SIZE, STDEV_SIZE,
                      RETENTION_TIME, upload_interval=5, total_uploads=10000,
-                     batch_size=100, connection=connection)
+                     batch_size=100, connection=connection, container=container)
 
 
 # Upload a large amount of objects intermittently, e.g. a live server that accepts batches of files from many clients
-def test_burst_workload(connection):
+def test_burst_workload(connection, container):
    run_workload_test(MEAN_SIZE, MAX_SIZE, MIN_SIZE, STDEV_SIZE,
                      RETENTION_TIME, upload_interval=1800, total_uploads=40,
-                     batch_size=25_000, connection=connection)
+                     batch_size=25_000, connection=connection, container=container)
 
 
 # Upload a huge amount of objects at once, e.g. a user is uploading a backup of their file-system.
-def test_all_at_once_workload(connection):
+def test_all_at_once_workload(connection, container):
    run_workload_test(MEAN_SIZE, MAX_SIZE, MIN_SIZE, STDEV_SIZE,
                      RETENTION_TIME, upload_interval=0, total_uploads=1,
-                     batch_size=1_000_000, connection=connection)
+                     batch_size=1_000_000, connection=connection, container=container)
